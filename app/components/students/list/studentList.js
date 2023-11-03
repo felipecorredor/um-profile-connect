94f467788bd1e33e3946d0163b367d6cad3998ef
Felipe Corredor <pipecorredor06@gmail.com>
2023-11-03 09:50:50 -0500

    counter app

--- app/components/students/list/studentList.js
+++ app/components/students/list/studentList.js
@@ -1,15 +1,26 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import StudentCard from "./studentCard";
+import { Button } from "react-bootstrap";
 
 const StudentList = ({ students }) => {
+  const [counter, setCounter] = useState(0);
+
   return (
-    <div className="row">
-      {students.map((student) => (
-        <StudentCard student={student} key={student.id} />
-      ))}
-    </div>
+    <>
+      <div className="row">
+        {students.map((student) => (
+          <StudentCard student={student} key={student.id} />
+        ))}
+      </div>
+      <div>
+        <Button onClick={() => setCounter((prevCounter) => prevCounter + 1)}>
+          Counter button
+        </Button>
+        <p>Counter: {counter}</p>
+      </div>
+    </>
   );
 };
 
